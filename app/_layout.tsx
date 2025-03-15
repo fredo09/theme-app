39860b544bf05979ca5263658156713c5bfae6cf
fredo09 <fred_15@outlook.com>
2025-03-15 03:17:41 -0600

    ad new component TextThemed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import { useFonts } from 'expo-font';
-import { Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import { ViewThemed } from '@/presentation/shared/ViewThemed';
+import { TextThemed } from '@/presentation/shared/TextThemed';
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 // import { Stack } from 'expo-router';
@@ -12,8 +14,6 @@ import * as SplashScreen from 'expo-splash-screen';
 //* Global css
 import './../global.css';
 import 'react-native-reanimated';
-import { useThemeColor } from '@/hooks/useThemeColor';
-import { ViewThemed } from '@/presentation/shared/ViewThemed';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -46,7 +46,9 @@ export default function RootLayout() {
         {/* <Stack>
         </Stack> */}
         <ViewThemed isMargin>
-          <Text className='mt-10 text-3xl dark:text-dark-text text-light-text'>Layout</Text>
+          <TextThemed className='mt-20'>
+            Layout Home
+          </TextThemed>
         </ViewThemed>
         <StatusBar style="auto" />
       </ThemeProvider>
---
+++ presentation/shared/TextThemed.tsx
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import { Text, TextProps } from 'react-native';
+
+type TypeText = 'h1' | 'h2' | 'normal' | 'semi-bold' | 'link';
+
+interface Props extends TextProps {
+    className?: string;
+    typeText?: TypeText;
+}
+
+export const TextThemed = ({ className, typeText, ...restProps }: Props) => {
+    // mt-10 text-3xl dark:text-dark-text text-light-text
+    return (
+        <Text
+            className={[
+                className,
+                'mt-10 text-3xl dark:text-dark-text text-light-text',
+                typeText === 'normal' ? 'font-normal' : undefined,
+                typeText === 'h1' ? 'text-3xl' : undefined,
+                typeText === 'h2' ? 'text-xl' : undefined,
+                typeText === 'semi-bold' ? 'font-semibold': undefined,
+                typeText === 'link' ? 'underline' : undefined,
+            ].join(' ')}
+            {...restProps} /> //! -> Forma de recupara el children
+    );
+};
