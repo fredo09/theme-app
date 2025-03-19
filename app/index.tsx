5949ddc9a0ced5a10fddad51c2eb35fcc783307d
fredo09 <fred_15@outlook.com>
2025-03-19 00:38:51 -0600

    change identation for file

--- app/index.tsx
+++ app/index.tsx
@@ -6,52 +6,52 @@ import { ViewThemed } from '@/presentation/shared/ViewThemed';
 import { animationMenuRoutes, menuRoutes, uiMenuRoutes } from '@/constants/Routes';
 
 const ComponentApp = () => {
-  return (
-    <ViewThemed isMargin>
-        {
-            animationMenuRoutes.map((route, idx) => (
-                <MenuItem 
-                    key={`${route.title}-${idx}`}
-                    title={route.title}
-                    name={route.name}
-                    icon={route.icon}
-                    isFirst={idx === 0}
-                    isLast={idx === animationMenuRoutes.length - 1}
+    return (
+        <ViewThemed isMargin>
+            {
+                animationMenuRoutes.map((route, idx) => (
+                    <MenuItem
+                        key={`${route.title}-${idx}`}
+                        title={route.title}
+                        name={route.name}
+                        icon={route.icon}
+                        isFirst={idx === 0}
+                        isLast={idx === animationMenuRoutes.length - 1}
                     />
-            ))
-        }
-
-        <View className='my-3'>
-            {
-                uiMenuRoutes.map((route, idx) => (
-                    <MenuItem 
-                        key={`${route.title}-${idx}`}
-                        title={route.title}
-                        name={route.name}
-                        icon={route.icon}
-                        isFirst={idx === 0}
-                        isLast={idx === animationMenuRoutes.length - 1}
-                        />
                 ))
             }
-        </View>
 
-        <View className='my-3'>
-            {
-                menuRoutes.map((route, idx) => (
-                    <MenuItem 
-                        key={`${route.title}-${idx}`}
-                        title={route.title}
-                        name={route.name}
-                        icon={route.icon}
-                        isFirst={idx === 0}
-                        isLast={idx === animationMenuRoutes.length - 1}
+            <View className='my-3'>
+                {
+                    uiMenuRoutes.map((route, idx) => (
+                        <MenuItem
+                            key={`${route.title}-${idx}`}
+                            title={route.title}
+                            name={route.name}
+                            icon={route.icon}
+                            isFirst={idx === 0}
+                            isLast={idx === animationMenuRoutes.length - 1}
                         />
-                ))
-            }
-        </View>
-    </ViewThemed>
-  )
+                    ))
+                }
+            </View>
+
+            <View className='my-3'>
+                {
+                    menuRoutes.map((route, idx) => (
+                        <MenuItem
+                            key={`${route.title}-${idx}`}
+                            title={route.title}
+                            name={route.name}
+                            icon={route.icon}
+                            isFirst={idx === 0}
+                            isLast={idx === animationMenuRoutes.length - 1}
+                        />
+                    ))
+                }
+            </View>
+        </ViewThemed>
+    )
 }
 
 export default ComponentApp;
