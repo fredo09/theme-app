b3f1e0250b7dc5a0e91a5ac039be8388d593cfef
fredo09 <fred_15@outlook.com>
2025-03-26 21:05:42 -0600

    add alert for component

--- app/alerts/index.tsx
+++ app/alerts/index.tsx
@@ -1,10 +1,45 @@
-import { View, Text } from 'react-native';
+import { Alert } from 'react-native';
+import { ButtonThemed } from '@/presentation/shared/ButtonThemed';
+import { ViewThemed } from '@/presentation/shared/ViewThemed';
 
 const AlertsScreen = () => {
+	const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+
+		const createThreeButtonAlert = () =>
+			Alert.alert('Quieres esto??', 'Recuerda que si quieres esto???', [
+				{
+					text: 'Ask me later',
+					onPress: () => console.log('Ask me later pressed'),
+				},
+				{
+					text: 'Cancel',
+					onPress: () => console.log('Cancel Pressed'),
+					style: 'destructive',
+				},
+				{text: 'OK', onPress: () => console.log('OK Pressed')},
+			]);
+
   return (
-    <View>
-      <Text>AlertsScreen</Text>
-    </View>
+    <ViewThemed isMargin className='justify-center items-center'>
+      <ButtonThemed
+				onPress={createTwoButtonAlert}
+				className='mb-5 p-2'>
+				Alert 1
+			</ButtonThemed>
+			<ButtonThemed
+				onPress={createThreeButtonAlert}
+				className='mb-5 p-2'>
+					Alert 2
+			</ButtonThemed>
+    </ViewThemed>
   );
 };
 export default AlertsScreen;
