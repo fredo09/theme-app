fe0a64626c3177c1dd9b57d34c58a1f743f60dbe
fredo09 <fred_15@outlook.com>
2025-03-31 21:51:40 -0600

    add component section list  adn interfaces

--- app/section-list/index.tsx
+++ app/section-list/index.tsx
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native';
+import { SectionList } from 'react-native';
+import { CardThemed } from '@/presentation/shared/CardThemed';
+import { TextThemed } from '@/presentation/shared/TextThemed';
+import { ViewThemed } from '@/presentation/shared/ViewThemed';
+
+import { housesI } from '@/infraestructure/interfaces/section-list-material';
 
 const SectionListScreen = () => {
   return (
-    <View>
-      <Text>SectionListScreen</Text>
-    </View>
+    <ViewThemed isMargin>
+      <CardThemed>
+				<SectionList
+					sections={housesI}
+					keyExtractor={(item, index) => item + index}
+					renderItem={({item}) => <TextThemed>{item}</TextThemed>}
+					ListHeaderComponent={() => (
+						<TextThemed typeText='h1' className='font-bold mb-3'>
+							Personajes
+						</TextThemed>
+					)}
+					renderSectionHeader={({section}) => (
+						<TextThemed typeText='h1' className='bg-light-background dark:bg-dark-background p-2 rounded'>{section.title}</TextThemed>
+					)}
+					ListFooterComponent={() => (
+						<TextThemed typeText='h2' className='font-bold bg-light-background dark:bg-dark-background p-2 mb-10 rounded'>
+							Section: {housesI.length}
+						</TextThemed>
+					)}
+					showsVerticalScrollIndicator={false}
+					stickySectionHeadersEnabled 
+				/>
+			</CardThemed>
+    </ViewThemed>
   );
 };
 export default SectionListScreen;
