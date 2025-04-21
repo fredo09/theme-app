eaa269d1cab1356bbf22488b3adfca80ac5348de
fredo09 <fred_15@outlook.com>
2025-04-20 22:52:53 -0600

    add initial component infiniteScroll with flatList

--- app/infinite-scroll/index.tsx
+++ app/infinite-scroll/index.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native';
+import { FlatList } from 'react-native';
+import { TextThemed } from '@/presentation/shared/TextThemed';
+import { ViewThemed } from '@/presentation/shared/ViewThemed';
 
 const InfiniteScrollScreen = () => {
   return (
-    <View>
-      <Text>InfiniteScrollScreen</Text>
-    </View>
+    <ViewThemed isMargin>
+      <FlatList
+				data={[1,2,3,4,5,6,7,8,9,10]}
+				renderItem={({item}) => (
+					<TextThemed>{item}</TextThemed>
+				)}
+			/>
+    </ViewThemed>
   );
 };
+
 export default InfiniteScrollScreen;
