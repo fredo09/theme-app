92c43e91afd77dc2cb89ac3a371800ab72d4bf12
fredo09 <fred_15@outlook.com>
2025-07-07 21:00:47 -0600

    formatear el código para mejorar la legibilidad en InfiniteScrollScreen y SectionListScreen

--- app/infinite-scroll/index.tsx
+++ app/infinite-scroll/index.tsx
@@ -3,16 +3,16 @@ import { TextThemed } from '@/presentation/shared/TextThemed';
 import { ViewThemed } from '@/presentation/shared/ViewThemed';
 
 const InfiniteScrollScreen = () => {
-  return (
-    <ViewThemed isMargin>
-      <FlatList
-				data={[1,2,3,4,5,6,7,8,9,10]}
-				renderItem={({item}) => (
+	return (
+		<ViewThemed isMargin>
+			<FlatList
+				data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+				renderItem={({ item }) => (
 					<TextThemed>{item}</TextThemed>
 				)}
 			/>
-    </ViewThemed>
-  );
+		</ViewThemed>
+	);
 };
 
 export default InfiniteScrollScreen;
--- app/section-list/index.tsx
+++ app/section-list/index.tsx
@@ -6,31 +6,37 @@ import { ViewThemed } from '@/presentation/shared/ViewThemed';
 import { housesI } from '@/infraestructure/interfaces/section-list-material';
 
 const SectionListScreen = () => {
-  return (
-    <ViewThemed isMargin>
-      <CardThemed>
+	return (
+		<ViewThemed isMargin>
+			<CardThemed>
 				<SectionList
 					sections={housesI}
 					keyExtractor={(item, index) => item + index}
-					renderItem={({item}) => <TextThemed>{item}</TextThemed>}
+					renderItem={({ item }) => <TextThemed>{item}</TextThemed>}
 					ListHeaderComponent={() => (
 						<TextThemed typeText='h1' className='font-bold mb-3'>
 							Personajes
 						</TextThemed>
 					)}
-					renderSectionHeader={({section}) => (
-						<TextThemed typeText='h1' className='bg-light-background dark:bg-dark-background p-2 rounded'>{section.title}</TextThemed>
+					renderSectionHeader={({ section }) => (
+						<TextThemed 
+							typeText='h1' 
+							className='bg-light-background dark:bg-dark-background p-2 rounded'>
+								{section.title}
+							</TextThemed>
 					)}
 					ListFooterComponent={() => (
-						<TextThemed typeText='h2' className='font-bold bg-light-background dark:bg-dark-background p-2 mb-10 rounded'>
+						<TextThemed 
+							typeText='h2' 
+							className='font-bold bg-light-background dark:bg-dark-background p-2 mb-10 rounded'>
 							Section: {housesI.length}
 						</TextThemed>
 					)}
 					showsVerticalScrollIndicator={false}
-					stickySectionHeadersEnabled 
+					stickySectionHeadersEnabled
 				/>
 			</CardThemed>
-    </ViewThemed>
-  );
+		</ViewThemed>
+	);
 };
 export default SectionListScreen;
