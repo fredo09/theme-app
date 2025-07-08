ecbb9244db3f859c35033a2f6cccbc1b59804418
fredo09 <fred_15@outlook.com>
2025-07-07 22:25:21 -0600

    agregar indicador de carga y color primario en InfiniteScrollScreen

--- app/infinite-scroll/index.tsx
+++ app/infinite-scroll/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { FlatList, Image } from 'react-native';
+import { ActivityIndicator, FlatList, Image, View } from 'react-native';
 import { ViewThemed } from '@/presentation/shared/ViewThemed';
+import { useThemeColor } from '@/hooks/useThemeColor';
 
 
 interface ListItemsPropsI {
@@ -9,6 +10,9 @@ interface ListItemsPropsI {
 
 const InfiniteScrollScreen = () => {
 	const [numbers, setNumbers] = useState<number[]>([1,2,3,4,5]);
+
+	//* Set Primary Color
+	const primaryColor = useThemeColor({}, 'primary');
 
 	//! Function to load more numbers when the end of the list is reached
 	const loadMoreNumbers = () => {
@@ -28,15 +32,20 @@ const InfiniteScrollScreen = () => {
 				)}
 				onEndReached={loadMoreNumbers} // Function to load more items
 				onEndReachedThreshold={0.6} // Threshold to trigger loading more items
+				ListFooterComponent={() => (
+					<View style={{height: 150, justifyContent: 'center'}}>
+						<ActivityIndicator size={40} color={primaryColor}/>
+					</View>
+				)}
 			/>
 		</ViewThemed>
 	);
 };
 
-const ListImages = ({numberItem}: ListItemsPropsI) => {
+const ListImages = ({ numberItem }: ListItemsPropsI) => {
 	return (
 		<Image
-			style={{ width: '100%', height: 400, margin: 10 }}
+			style={{ width: '100%', height: 400}}
 			source={{ uri: `https://picsum.photos/200/300?random=${numberItem}` }}
 			resizeMode="cover"
 			accessibilityLabel={`Image number ${numberItem}`}
