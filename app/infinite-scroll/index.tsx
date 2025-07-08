2814d3c9326f76a768e719f197b980fafb16efcf
fredo09 <fred_15@outlook.com>
2025-07-07 22:18:39 -0600

    add picture fetch infinite scroll

--- app/infinite-scroll/index.tsx
+++ app/infinite-scroll/index.tsx
@@ -1,18 +1,48 @@
-import { FlatList } from 'react-native';
-import { TextThemed } from '@/presentation/shared/TextThemed';
+import { useState } from 'react';
+import { FlatList, Image } from 'react-native';
 import { ViewThemed } from '@/presentation/shared/ViewThemed';
 
+
+interface ListItemsPropsI {
+	numberItem: number;
+}
+
 const InfiniteScrollScreen = () => {
+	const [numbers, setNumbers] = useState<number[]>([1,2,3,4,5]);
+
+	//! Function to load more numbers when the end of the list is reached
+	const loadMoreNumbers = () => {
+		const loadNumbers = Array.from({length: 5}, (_,i) => numbers.length + i)
+
+		setTimeout(() => {
+			setNumbers((prev) => [...prev, ...loadNumbers]);
+		}, 3000);
+	}
+
 	return (
 		<ViewThemed isMargin>
 			<FlatList
-				data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+				data={numbers}
 				renderItem={({ item }) => (
-					<TextThemed>{item}</TextThemed>
+					<ListImages numberItem={item}/>
 				)}
+				onEndReached={loadMoreNumbers} // Function to load more items
+				onEndReachedThreshold={0.6} // Threshold to trigger loading more items
 			/>
 		</ViewThemed>
 	);
 };
+
+const ListImages = ({numberItem}: ListItemsPropsI) => {
+	return (
+		<Image
+			style={{ width: '100%', height: 400, margin: 10 }}
+			source={{ uri: `https://picsum.photos/200/300?random=${numberItem}` }}
+			resizeMode="cover"
+			accessibilityLabel={`Image number ${numberItem}`}
+			alt={`Image number ${numberItem}`}
+		/>
+	)
+}
 
 export default InfiniteScrollScreen;
